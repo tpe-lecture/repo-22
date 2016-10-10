d4a88896d3539895b3c2759a0fc4979856628cff
PhilipOe1612340 <1612340@stud.hs-mannheim.de>
2016-10-10 16:10:47 +0200

    no message

--- 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
+++ 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
@@ -84,8 +84,12 @@ public class ComplexNumberTest {
         // (3   +  5i) --conj--> (3   + -5i)
         // (-2  + -4i) --conj--> (-2  + 4i)
 
-        // TODO: Test schreiben
-    }
+        ComplexNumber a = new ComplexNumber(3, 5);
+        ComplexNumber b = new ComplexNumber(-2, -4);
+
+        assertEquals(new ComplexNumber(3, -5), a.conjugate());
+        assertEquals(new ComplexNumber(-2, 4), b.conjugate());
+     }
 
     /**
      * Testet die Berechnung des Betrages.

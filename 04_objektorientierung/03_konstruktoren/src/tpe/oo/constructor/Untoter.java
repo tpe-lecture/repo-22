329b0f2cafa3e1946a8c36c90edadcd531e22e24
PhilipOe1612340 <1612340@stud.hs-mannheim.de>
2016-10-24 14:43:44 +0200

    no message

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Untoter.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Untoter.java
@@ -16,7 +16,7 @@ public class Untoter  extends Wesen  {
     // Konstruktoren fehlen. Bitte hier einfügen.
     public Untoter(String name) {
         super(name);
-        this.unterwasseratmung=this.STANDARD_UNTERWASSERATMUNG;
+        this.unterwasseratmung=Untoter.STANDARD_UNTERWASSERATMUNG;
     }
 
     public Untoter(String name, int staerke){
--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
@@ -1,38 +1,50 @@
 package tpe.oo.polymorphie;
 
-import tpe.oo.polymorphie.tiere.Affe;
-import tpe.oo.polymorphie.tiere.Giraffe;
-import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Fütterung der Tiere.
  */
 public class Futterstelle {
+    /**
+     *
+     * @param tier zu fütterndes Tier
+     */
+    public static void gibFutter(ZooTier[] tier) {
+        for (int i = 0; i < tier.length; i++) {
+            tier[i].fuettern();
+        }
+    }
 
-    /**
-     * Füttert den Affen.
-     * 
-     * @param affe Affe, der gefüttert werden soll. 
-     */
-    public void gibFutter(Affe affe) {
-        affe.fuettern();
-    }
-    
-    /**
-     * Füttert den Gorilla.
-     * 
-     * @param gorilla Gorilla, der gefüttert werden soll. 
-     */
-    public void gibFutter(Gorilla gorilla) {
-        gorilla.fuettern();
-    }
-    
-    /**
-     * Füttert die Giraffe.
-     * 
-     * @param giraffe Giraffe, die gefüttert werden soll. 
-     */
-    public void gibFutter(Giraffe giraffe) {
-        giraffe.fuettern();
-    }
+
+
+
+
+
+//    /**
+//     * Füttert den Affen.
+//     *
+//     * @param affe Affe, der gefüttert werden soll.
+//     */
+//    public void gibFutter(Affe affe) {
+//        affe.fuettern();
+//    }
+//
+//    /**
+//     * Füttert den Gorilla.
+//     *
+//     * @param gorilla Gorilla, der gefüttert werden soll.
+//     */
+//    public void gibFutter(Gorilla gorilla) {
+//        gorilla.fuettern();
+//    }
+//
+//    /**
+//     * Füttert die Giraffe.
+//     *
+//     * @param giraffe Giraffe, die gefüttert werden soll.
+//     */
+//    public void gibFutter(Giraffe giraffe) {
+//        giraffe.fuettern();
+//    }
 }

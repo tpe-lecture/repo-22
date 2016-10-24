f2c6e835ab777af2aa8715f5102333a6d50c0644
PhilipOe1612340 <1612340@stud.hs-mannheim.de>
2016-10-24 11:39:57 +0200

    no message

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
@@ -3,19 +3,28 @@ package tpe.oo.constructor;
 /**
  * Nachtelf aus der Allianz.
  */
-public class Nachtelf /* extends Figur */ {
+public class Nachtelf  extends Wesen {
+
+
 
     /** Standardmäßige Stärke der Fähigkeit Naturwiderstand. */
     public static final int STANDARD_NATURWIDERSTAND = 5;
-    
+
     /** Fähigkeit zum Widerstand gegen Naturmagie */
     private int naturwiderstand;
-    
+
     // Konstruktoren fehlen. Bitte hier einfügen.
-    
+    public Nachtelf(String name) {
+        super(name);
+        this.naturwiderstand=Nachtelf.STANDARD_NATURWIDERSTAND;
+    }
+    public Nachtelf(String name, int staerke){
+        this(name);
+        this.naturwiderstand=staerke;
+    }
     /**
      * Stärke des Naturwiderstandes.
-     * 
+     *
      * @return the naturwiderstand
      */
     public int getNaturwiderstand() {

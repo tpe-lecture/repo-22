48f9593fbc4bbab9d920e40f8fe25009c790657e
Philip Oesterlin <1612340@stud.hs-mannheim.de>
2016-12-13 16:09:16 +0100

    Testat Korrektur
    auf Grund eines Fehlers bei der Syncronisation konnte ich die geänderte
    Datei gestern nicht hochladen und musste daher alles nochmal machen.
    Jetzt sollte es aber funktionieren.

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,104 +1,142 @@
 package tpe.testate.crypter;
 
 /**
- * Klasse zur sicheren Ver- und Entschlüsselung von Hochsicherheitsgeheimnissen.
+ * Klasse zur sicheren Ver- und Entschlüsselung von
+ * Hochsicherheitsgeheimnissen.
  */
 public class CrypterImpl implements Crypter {
-  String input;
-  String output;
-  char temp;
+	String input;
+	String output;
+	char temp;
 
-  @Override
-    public String encrypt(String input) throws IllegalArgumentException {
-    output = "";
-    input = input.toLowerCase();
-    for (int j = 0; j < input.length(); j++) {          //für jeden zu verschlüsselnden Buchstaben:
-      char temp = input.charAt(j);
-      int ascii = temp;
-      if ((ascii >= 97 && ascii <= 122)                 // Kleine Buchstaben
-             || (ascii >= 48 && ascii <= 57)            // Zahlen
-             || ascii == 32) {                          //Leerzeichen
+	/**
+	 * Methode zur Verschluesselng.
+	 *
+	 * @param input
+	 *            zu verschluesselnder String
+	 */
+	@Override
+	public String encrypt(String input) throws IllegalArgumentException {
+		output = "";
+		input = input.toLowerCase();
+		input = input.toLowerCase();
+		for (int j = 0; j < input.length(); j++) { // fuer jeden zu
+													// verschluesselnden
+													// Buchstaben:
+			char temp = input.charAt(j);
+			int ascii = temp;
+		      if ((temp >= 'a' && temp <= 'z') // Kleine Buchstaben
+	                    || (temp >= '0' && temp <= '9') // Zahlen
+	                    || temp == ' ') { // Leerzeichen
 
-        output = output + aendern(temp);                //Möglicherweise geändertes Zeichen anhängen
-      } else {
-        throw new IllegalArgumentException();
-      }
-    }
-    return output;
-  }
+				output = output + aendern(temp); // Moeglicherweise gaendertes
+													// Zeichen anhaengen
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
+		return output;
+	}
 
-  @Override
-    public String decrypt(String input) throws IllegalArgumentException {
-    if (input.equals(input.toLowerCase())) {                //falls sich nichts ändert,
-      return encrypt(input);                                    //sind alle Buchstaben klein.
-    } else {
-      throw new IllegalArgumentException();
-    }
-  }
+	  /**
+	   * Methode zur Entschluesselng.
+	   *
+	   * @param input zu entschluesselnder String
+	   */
+	@Override
+	public String decrypt(String input) throws IllegalArgumentException {
+	    if (input.equals(input.toLowerCase()) && checkForInvalicChar(input)) {
+	      // falls sich nichts aendert,
+	      return encrypt(input); // sind alle Buchstaben klein.
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+	/**
+	 * Ueberprueft beim entschluesseln ob der Buchstabe unterstuetzt wird.
+	 * @param test der zu testende Buchstabe
+	 * @return	gibt zurueck ob der Buchstabe unterstuetzt wird.
+	 * @throws IllegalArgumentException
+	 */
+	 private boolean checkForInvalicChar(String test) throws IllegalArgumentException {
+		    test = test.toLowerCase();
+		    for (int j = 0; j < test.length(); j++) { // fuer jeden zu verschluesselnden Buchstaben:
+		      char temp = test.charAt(j);
+		      if ((temp >= 'a' && temp <= 'z') // Kleine Buchstaben
+		                        || (temp >= '0' && temp <= '9') // Zahlen
+		                        || temp == ' ') { // Leerzeichen
+		      } else {
+		        throw new IllegalArgumentException();
+		      }
+		    }
+		    return true;
+		  }
 
-  /**
-  * Ändert alle angegebenen Buchstaben auf den dazugehöhrigen Partner.
-  *  <code>
-  *    e -> 3
-  *    3 -> e
-  *    l -> 1
-  *    1 -> l
-  *    o -> 0
-  *    0 -> o
-  *    a -> 4
-  *    4 -> a
-  *    t -> 7
-  *    7 -> t
-  *  </code>
-  * @param ch zu überprüfender Buchstabe
-  * @return möglicherweise geänderter Buchstabe
-   */
-  char aendern(char ch) {
-    switch (ch) {
-      case 'e': {
-        ch = '3';
-        break;
-      }
-      case '3': {
-        ch = 'e';
-        break;
-      }
-      case 'l': {
-        ch = '1';
-        break;
-      }
-      case '1': {
-        ch = 'l';
-        break;
-      }
-      case 'o': {
-        ch = '0';
-        break;
-      }
-      case '0': {
-        ch = 'o';
-        break;
-      }
-      case 'a': {
-        ch = '4';
-        break;
-      }
-      case '4': {
-        ch = 'a';
-        break;
-      }
-      case '7': {
-        ch = 't';
-        break;
-      }
-      case 't': {
-        ch = '7';
-        break;
-      }
 
-      default:
-        break;
-    }
-    return ch;
-  }
+	 /**
+	   * Aendert alle angegebenen Buchstaben auf den dazugehoehrigen Partner. <code>
+	   *    e -> 3
+	   *    3 -> e
+	   *    l -> 1
+	   *    1 -> l
+	   *    o -> 0
+	   *    0 -> o
+	   *    a -> 4
+	   *    4 -> a
+	   *    t -> 7
+	   *    7 -> t
+	   *  </code>
+	   *
+	   * @param ch zu Ueberpruefender Buchstabe
+	   * @return moeglicherweise geaenderter Buchstabe
+	   */
+	  private char aendern(char ch) {
+		switch (ch) {
+		case 'e':
+			ch = '3';
+			break;
+
+		case '3':
+			ch = 'e';
+			break;
+
+		case 'l':
+			ch = '1';
+			break;
+
+		case '1':
+			ch = 'l';
+			break;
+
+		case 'o':
+			ch = '0';
+			break;
+
+		case '0':
+			ch = 'o';
+			break;
+
+		case 'a':
+			ch = '4';
+			break;
+
+		case '4':
+			ch = 'a';
+			break;
+
+		case '7':
+			ch = 't';
+			break;
+
+		case 't':
+			ch = '7';
+			break;
+
+
+		default:
+			break;
+		}
+
+		return ch;
+	}
 }
